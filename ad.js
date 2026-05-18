6728d6fbabb3953fbad637cc83487de94d430554
Ilfiyah Ilma <topiposodino@hotmail.com>
2026-05-18 16:25:59 +0700

    Create ad.js

---
+++ ad.js
@@ -0,0 +1,12 @@
+document.write(`
+<script>
+  atOptions = {
+    'key' : '71f6a907de8fd53a37ae9157dc36380a',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+<\/script>
+<script src="//compiledonatevanity.com/71f6a907de8fd53a37ae9157dc36380a/invoke.js"><\/script>
+`);
